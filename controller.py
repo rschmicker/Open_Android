a1ae99af87f2a9b763005a7618650758819b6ae8
rschmicker <rhschmicker@gmail.com>
2016-07-25 18:15:50 -0400

    Moved to thread pool

--- controller.py
+++ controller.py
@@ -10,20 +10,38 @@ from json_builder import *
 from solr_api import *
 from multiprocessing.dummy import Pool as ThreadPool
 
+class controller:
+    completed_apks = []
+    def __init__(self):
+        self.apk_list = self.get_APK_list(variables.apk_dir)
+        self.threadPool(self.apk_list, 4)
 
-def threadPool(apks, threads=2):
-    pool = ThreadPool(threads)
-    results = pool.map(extractor, apks)
-    pool.close()
-    pool.join()
-    return results
-    
-def extractor(apk):
-    a = appinfo(apk)
-    p = permparser(apk)
-    i = intentparser(apk)
-    com = Sample(apk)
-    s = stringparser(apk)
-    ap = apiparser(apk)
-    mongo_db(a, p, i, s, ap, com)
-    j = json_builder(a, p, i, s, ap, com)
+    def get_APK_list(self, APK_dir):
+        apks = []
+        for root, dirs, files in os.walk(APK_dir, topdown=False):
+            for name in files:
+                if name.endswith(".apk"):
+                    apks.append(os.path.join(root, name))      
+        return apks
+
+    def threadPool(self, apks, threads=2):
+        pool = ThreadPool(threads)
+        results = pool.map(self.extractor, apks)
+        pool.close()
+        pool.join()
+        return results
+        
+    def extractor(self, apk):
+        a = appinfo(apk)
+        p = permparser(apk)
+        i = intentparser(apk)
+        com = Sample(apk)
+        s = stringparser(apk)
+        ap = apiparser(apk)
+        mongo_db(a, p, i, s, ap, com)
+        j = json_builder(a, p, i, s, ap, com)
+        self.completed_apks.append(a.APK_name)
+        self.completionRate()
+
+    def completionRate(self):
+        print(str(len(self.completed_apks)/len(self.apk_list)*100) + "%")
--- main.py
+++ main.py
@@ -1,33 +1,13 @@
 import subprocess
-import controller
+from controller import *
 import threading
 import time
 import variables
+import os
 from multiprocessing.dummy import Pool as ThreadPool
-
-def get_APK_list(APK_dir):
-		list_apk_cmd = "ls " + APK_dir + " | grep .apk"
-		list_subp = subprocess.Popen(['/bin/sh', '-c', list_apk_cmd], stdout=subprocess.PIPE)
-		out = list_subp.communicate()[0]
-		apk_list = []
-		for line in out.splitlines():
-			if line.find(" ") != -1:
-				line = "'" + line + "'"
-				apk_modify = "mv " + APK_dir + line + " " + APK_dir + line.replace(" ","_")
-				list_subp = subprocess.Popen(['/bin/sh', '-c', apk_modify], stdout=subprocess.PIPE)
-				line = line.replace(" ","_")
-			apk_list.append(line)
-		return apk_list
-
+	
 def main():
-	APK_dir = variables.apk_dir
-	APK_list = get_APK_list(APK_dir)
-	for i in range(len(APK_list)):
-		APK_list[i] = APK_dir + APK_list[i]
-
-	output = controller.threadPool(APK_list, 4)
-	print(output)
-
+	c = controller()
 
 start_time = time.time()
 main()

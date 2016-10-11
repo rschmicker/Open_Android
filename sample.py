9c4b86407ab621faa094e454623c5176668d7dd1
rschmicker <rhschmicker@gmail.com>
2016-10-11 17:09:41 -0400

    Added complex query

--- sample.py
+++ sample.py
@@ -1,7 +1,6 @@
 import urllib2
 
-host = "localhost"
-port = "8080"
+host_port = "http://localhost:8080"
 
 def call_and_print(url):
     doc = urllib2.urlopen(url)
@@ -9,13 +8,23 @@ def call_and_print(url):
         print(line)
 
 #package_name and permissions with an output of a csv
-pn_p_c = "http://" + host + ":" + port + "/solr/apps/select?q=*:*&fl=package_name+permissions&wt=csv"
+pn_p_c = (host_port + '/solr/apps/select?'
+                    'q=*:*&fl=package_name+permissions&wt=csv')
 
 #package_name and permissions with an output of a json
-pn_p_j = "http://" + host + ":" + port + "/solr/apps/select?q=*:*&fl=package_name+permissions&wt=json"
+pn_p_j = (host_port + '/solr/apps/select?'
+                    'q=*:*&fl=package_name+permissions&wt=json')
 
 #package_name and permissions with an output of a xml
-pn_p_x = "http://" + host + ":" + port + "/solr/apps/select?q=*:*&fl=package_name+permissions&wt=xml"
+pn_p_x = (host_port + '/solr/apps/select?'
+                    'q=*:*&fl=package_name+permissions&wt=xml')
+
+# complex query extracting package_name and permissions
+# but only APK's containing the android.permission.CAMERA,
+# omits the header, return type of a csv, and limits the
+# response to just 2 rows
+complex_q = (host_port + '/solr/apps/select?'
+    'q=*.CAMERA&omitHeader=true&fl=package_name+permissions&rows=2&wt=csv')
 
 print("====================================================")
 print("CSV:")
@@ -31,3 +40,8 @@ print("====================================================")
 print("XML:")
 print("====================================================")
 call_and_print(pn_p_x)
+
+print("====================================================")
+print("Complex:")
+print("====================================================")
+call_and_print(complex_q)
